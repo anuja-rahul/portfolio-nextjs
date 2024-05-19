2f83a06ef74dc30369e5f6383185239b0c0e6fa9
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-05-19 16:43:41 +0530

    fix

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,12 +3,15 @@ import "./../index.scss";
 export default function Hero() {
   return (
     <section id="Hero" className="min-h-screen">
-      <div className="section-container flex flex-col flex-wrap items-center justify-center">
+      <div className="section-container flex flex-row flex-wrap items-center justify-center z-30">
         <h1 className="title text-balance text-center font-bold text-white">
           Hello👋 I{"'"}m <span className="cool-letters">A</span>
           nuja Rahul
           <br />
           Web developer
+        </h1>
+        <h1 className="title text-balance text-center font-bold text-white absolute">
+          {/* This is content */}
         </h1>
       </div>
     </section>
