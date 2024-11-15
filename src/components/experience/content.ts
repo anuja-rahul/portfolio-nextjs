c2133a4cba5b3ed35aaa47ef334cdf7d130754f0
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-11-16 00:19:27 +0530

    changes

--- src/components/experience/content.ts
+++ src/components/experience/content.ts
@@ -4,7 +4,7 @@ export const Experiences = [
     role: "Computer Science Undergraduate",
     Institution: "IIT(Uow)",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nostrum nesciunt laborum nulla quisquam ullam reprehenderit molestias amet officiis optio dolor, quasi, consectetur itaque quod tempore et veniam ab quam?",
+      "",
     tecnologies: ["Python", "Java", "HTML", "CSS", "Javascript"],
   },
   {
@@ -12,7 +12,7 @@ export const Experiences = [
     role: "Frontend Developer",
     Institution: "Freelance",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit adipisicing elit.",
+      "",
     tecnologies: [
       "Javascript",
       "CSS",
@@ -31,7 +31,7 @@ export const Experiences = [
     role: "Python Developer",
     Institution: "Open Source",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. amet consectetur adipisicing amet consectetur adipisicing. Lorem ipsum dolor sit amet consectetur adipisicing elit. amet consectetur adipisicing amet consectetur adipisicing.",
+      "",
     tecnologies: ["Python", "MariaDB"],
   },
   {
@@ -39,7 +39,7 @@ export const Experiences = [
     role: "GCE AL's",
     Institution: "Bandaranayake College",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit adipisicing elit. Ad nostrum nesciunt laborum nulla quisquam ullam reprehenderit.",
+      "",
     tecnologies: ["Python", "Arduino", "SQL", "HTML", "CSS"],
   },
 ];
